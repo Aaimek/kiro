9d55204b8a880da0704aa76a8ee62c8d3c4d3292
Mathias Martinez <you@example.com>
2022-11-24 19:40:58 +0100

    update

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ import pandas as pd
 f = open('instances/tiny.json')
 data = json.load(f)
 
-L = [1, 2, 3]
+
+
 
 def duree_liste_task(tasks):
     tasks_info = data['tasks']
@@ -15,6 +16,8 @@ def duree_liste_task(tasks):
     
     return temps
 
+
+
 def duree_reste_job(job, derniere_task = 0):
     dic_job = data['jobs'][job-1]
     liste = dic_job['sequence'][derniere_task:]
@@ -23,4 +26,55 @@ def duree_reste_job(job, derniere_task = 0):
     return temps
 
 
-print(duree_reste_job(1, 2))
+
+def job_fini_a_temps(job, jour, derniere_task):
+    jobs_dic = data['jobs'][job-1]
+    due_date = jobs_dic['due_date']
+
+    depass =    due_date - (jour + duree_reste_job(job, derniere_task)) 
+    return depass >= 0
+
+
+
+def penal_decal_job(jour, job):
+    penal = 0
+    jobs_dic = data['jobs'][job-1]
+    weight = jobs_dic['weight']
+    due_date = jobs_dic['due_date']
+
+    if jour > due_date:
+        penal = weight *(jour - (due_date) + 6)
+
+    return penal
+
+def find_job_task(task):
+    jobs_dic = data['jobs']
+
+    for k in range(len(jobs_dic)):
+        if task in jobs_dic[k]['sequence']:
+            return k+1
+
+
+def classement(L_tasks, jour):
+    res = {}
+
+
+    for task in L_tasks:
+        score = 1000
+        task_fait = 
+        job = find_job_task(task)
+        duree_finir_job = duree_reste_job(job, task_fait)
+        
+        if job_fini_a_temps(job, jour, task_fait):
+            score -= 3
+
+        weight = data['jobs'][job-1]['weight']
+        penal_decal_job = penal_decal_job(jour, job)
+
+        if  3 > penal_decal_job > 0:
+            score += 10
+
+        score = weight + penal_decal_job*0.4
+        res[task] = score
+       
+    
