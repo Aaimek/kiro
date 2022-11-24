430db1ed4e98fc3a85cd0f3dcc6289162cced1f2
Mulder Nolan <nolan.mulder@student-cs.fr>
2022-11-24 15:51:15 +0100

    ajout de update_tasks

---
+++ update_tasks.py
@@ -0,0 +1,33 @@
+
+# Exemple des données en entrée:
+
+# tasks_en_cours = [
+#     {
+#         'task': 3,
+#         'temps-avant-fin': 2,
+#         'machine': 7,
+#         'operateur': 9
+#     },
+#     {
+#         'task': 5,
+#         'temps-avant-fin': 1,
+#         'machine': 4,
+#         'operateur': 11
+#     }
+# ]
+
+# tasks_finies = [1, 2]
+
+# mets a jours les taches finies et le temps restant des taches
+def update_tasks(task_finies, tasks_en_cours):
+    for task in tasks_en_cours:
+        # si elle vas finir, on la mets dans les tasks finies
+        if task['temps-avant-fin']==1:
+            task_finies.append(task)
+            tasks_en_cours.remove(task)
+
+        # si elle continue, on met a jour le temps
+        else:
+            task['temps-avant-fin'] -= 1
+
+        return task_finies, tasks_en_cours
